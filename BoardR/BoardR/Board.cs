a3b30d1e95f0f941a40dbc45fc56d86e240c5f5b
Hristian-N <hristian.kr.nikolov@gmail.com>
2023-09-14 15:51:42 +0300

    Done with BoardR Part 1

--- BoardR/BoardR/Board.cs
+++ BoardR/BoardR/Board.cs
@@ -3,9 +3,4 @@ using System.ComponentModel;
 static class Board
 {
     public static List<BoardItem> items = new List<BoardItem>();
-
-    public static void Add(BoardItem item)
-    {
-        items.Add(item);
-    }
 }
--- BoardR/BoardR/Program.cs
+++ BoardR/BoardR/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var item = new BoardItem("Refactor this mess", DateTime.Now.AddDays(2));
+item.AdvanceStatus();
+var anotherItem = new BoardItem("Encrypt user data", DateTime.Now.AddDays(10));
+
+Board.items.Add(item);
+Board.items.Add(anotherItem);
+
+foreach (var boardItem in Board.items)
+{
+    boardItem.AdvanceStatus();
+}
+
+foreach (var boardItem in Board.items)
+{
+    Console.WriteLine(boardItem.ViewInfo());
+}
+
+Console.ReadLine(); // to pause console and not exit
