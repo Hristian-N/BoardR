6dc50762e96b9c90c3e9f5d0acc1c3523a7e79aa
Hristian-N <hristian.kr.nikolov@gmail.com>
2023-09-28 15:49:35 +0300

    Updated BoardR project

---
+++ Boarder/Loggers/ConsoleLogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Boarder.Loggers
+{
+    public class ConsoleLogger : ILogger
+    {
+
+        public void Log(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
+
---
+++ Boarder/Models/Issue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Boarder.Models
+{
+    public class Issue : BoardItem
+    {
+        public Issue(string title, string description, DateTime dueDate)
+            : base(title, dueDate, Status.Open)
+        {
+            this.Description = description ?? "No desciption";
+
+            this.AddEventLog($"Created Issue: {this.ViewInfo()}. Description: {this.Description}");
+        }
+
+        public string Description { get; }
+
+        public override void AdvanceStatus()
+        {
+            if (this.Status != Status.Verified)
+            {
+                this.Status = Status.Verified;
+                this.AddEventLog("Issue status set to Verified");
+            }
+            else
+            {
+                this.AddEventLog("Issue status already Verified");
+            }
+        }
+
+        public override string ViewInfo()
+        {
+            var baseInfo = base.ViewInfo();
+
+            // output info about the Type, the baseInfo, the Assignee
+            return $"{baseInfo} Description: {Description}";
+        }
+
+        public override void RevertStatus()
+        {
+            if (this.Status != Status.Open)
+            {
+                this.Status = Status.Open;
+                this.AddEventLog("Issue status set to Open");
+            }
+            else
+            {
+                this.AddEventLog("Issue status already Open");
+            }
+        }
+    }
+}
